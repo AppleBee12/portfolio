64a6126daa252b51a0dd68de161e496488ed43b8
AppleBee12 <mieldeluna@naver.com>
2024-11-22 21:35:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        Hello, World!
+        계속 업데이트 될 예정입니다!
+      </div>
+    );
+  }
+}
+
+
+export default App;
